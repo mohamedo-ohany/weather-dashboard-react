430494c24f96442b01aa9fc73dc2a5bde2f08389
mohamed hany halima <155679248+mohamedo-ohany@users.noreply.github.com>
2026-04-26 20:09:17 +0300

    Update i18n.ts

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -22,6 +22,9 @@ i18n
   .init({
     fallbackLng: 'en',
     debug: false,
+        backend: {
+      loadPath: '/locales/{{lng}}/{{ns}}.json',
+    },
 
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
